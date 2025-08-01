7e305d18c7701e1fe2a8991f3ea8653c91daece7
Seemab Ahmed <seemab40615@gmail.com>
2025-08-02 03:35:23 +0500

    fix

--- libs/sheetsClient.js
+++ libs/sheetsClient.js
@@ -1,14 +1,15 @@
 import { google } from "googleapis";
-import { readFileSync } from "fs";
-import path from "path";
 
 export async function getSheetsClient() {
   const auth = new google.auth.GoogleAuth({
-    keyFile: path.join(process.cwd(), "credentials/google-service-account.json"),
+    credentials: {
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      private_key: process.env.GOOGLE_SERVICE_ACCOUNT_KEY?.replace(/\\n/g, '\n'),
+    },
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
   });
 
   const authClient = await auth.getClient();
   const sheets = google.sheets({ version: "v4", auth: authClient });
   return sheets;
-}   
+}
