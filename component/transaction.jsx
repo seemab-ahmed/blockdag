ac1ee860c076e6bbca4a0f69ebc3a69b623543f1
Seemab Ahmed <seemab40615@gmail.com>
2025-08-03 02:35:17 +0500

    fix

--- component/transaction.jsx
+++ component/transaction.jsx
@@ -1,5 +1,6 @@
 'use client'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
 import { useSheetData } from '../hooks/useSheetData';
 import { parseSheetData } from '../utils/sheetParser';
 
